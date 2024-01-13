0d0377adacfbe5ce12e7c955bb5ed6b0238f113c
Ayşenur <aysenur_can_@hotmail.com>
2024-01-13 15:49:19 +0300

    endpoint connection for user data retrieval added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,17 @@ import Link from 'next/link'
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 
-const rows = [
-  {
-    id: 1,
-    name: "Nannie",
-    surname: "Steuber",
-    birthday: "1948-05-18T17:43:31.739Z",
-    email: "Meta"
-  },
-  {
-    id: 2,
-    name: "Neuberie",
-    surname: "Sniber",
-    birthday: "1948-05-18T17:43:31.739Z",
-    email: "Amazon"
-  }, {
-    id: 3,
-    name: "Natiet",
-    surname: "Stier",
-    birthday: "1948-05-18T17:43:31.739Z",
-    email: "Google"
-  },
-];
+export type User = {
+  id: number,
+  name: string,
+  surname: string,
+  email: string,
+  birthday: string,
+}
 
 export default async function Home() {
-  // const staticData = await fetch(`https://63b49a269f50390584b53ecf.mockapi.io/users`, { cache: 'force-cache' })
+  const userResponse = await fetch(`http://localhost:8080/users`, { cache: 'force-cache' })
+  const users = (await userResponse.json()).data as User[]
 
   return (
     <TableContainer className='user-table' sx={{ mx: 'auto', width: '75%', height: '65%' }} component={Paper}>
@@ -43,18 +29,18 @@ export default async function Home() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {users.map((user: User) => (
             <TableRow
-              key={row.name}
+              key={user.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                1
+                {user.id}
               </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.surname}</TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-              <TableCell align="right">{row.birthday}</TableCell>
+              <TableCell align="right">{user.name}</TableCell>
+              <TableCell align="right">{user.surname}</TableCell>
+              <TableCell align="right">{user.email}</TableCell>
+              <TableCell align="right">{user.birthday}</TableCell>
               <TableCell align="right">
                 <IconButton aria-label="edit" color="primary">
                   <EditIcon />
